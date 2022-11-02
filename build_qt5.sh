e2013d0ae4fb9be8b1f1b1af41bb6590da8d3048
Philip Seeger <philip@c0xc.net>
2022-11-02 11:39:34 +0100

    test

--- build_qt5.sh
+++ build_qt5.sh
@@ -23,11 +23,11 @@ function fetch_qt {
     if [ ! -d "$SRC_DIR" ]; then
 
         if [ ! -f "qt-everywhere-src-$QT_VERSION.tar.xz" ]; then
-            wget --no-check-certificate https://download.qt.io/archive/qt/$QT_MAJOR.$QT_MINOR/$QT_VERSION/single/qt-everywhere-src-$QT_VERSION.tar.xz || return $?
+            wget -nv --no-check-certificate https://download.qt.io/archive/qt/$QT_MAJOR.$QT_MINOR/$QT_VERSION/single/qt-everywhere-src-$QT_VERSION.tar.xz || return $?
         fi
 
         if [ ! -f "md5sums.txt" ]; then
-            wget https://download.qt.io/archive/qt/$QT_MAJOR.$QT_MINOR/$QT_VERSION/single/md5sums.txt
+            wget -nv https://download.qt.io/archive/qt/$QT_MAJOR.$QT_MINOR/$QT_VERSION/single/md5sums.txt
         fi
         # --ignore-missing missing
         #md5sum -c md5sums.txt
--- build_src.sh
+++ build_src.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#
+# GITHUB BUILD PIPELINE - BUILD SCRIPT
 
 if [[ -z "$GITHUB_WORKSPACE" ]]; then
     echo "workspace directory missing" >&2
@@ -11,49 +13,67 @@ if [ -n "$INPUT_RECIPE" ]; then
     source "$INPUT_RECIPE"
 fi
 
-# Load Qt
-echo "build pipeline - application build script..."
+# Load Qt (custom Qt build if available, standard Qt installation otherwise)
+echo "BUILD PIPELINE - APPLICATION BUILD SCRIPT..."
 ls -l /src /build
 if [ -f "/etc/profile.d/qt.sh" ]; then
     echo "found Qt env script, sourcing it..."
     cat /etc/profile.d/qt.sh
     source /etc/profile.d/qt.sh
 fi
-echo "QTDIR: $QTDIR"
+if [ -n "$QTDIR" ]; then
+    echo "QTDIR: $QTDIR"
+else
+    echo "QTDIR not defined, using Qt installation from distribution" >&2
+fi
 if ! which qmake >/dev/null 2>&1; then
     echo "qmake missing!" >&2
 fi
+echo -n "= "
 which qmake || exit $?
+echo
 
 # Prepare linuxdeploy tool for creating AppImage
+# It should be included in the Docker container, but if not, download it...
 linuxdeploy=linuxdeploy-x86_64.AppImage
 if ! which $linuxdeploy >/dev/null 2>&1; then
     # TODO download current version if vars[DOWNLOAD_LINUXDEPLOY] ...
+    # Copy (from Docker image) or (try to) download linuxdeploy tool
     echo "getting deploy tool: $linuxdeploy"
     if [ -f "/var/tmp/$linuxdeploy" ]; then
-        ln -s /var/tmp/$linuxdeploy /usr/local/bin/
+        (cd /usr/local/bin && cp -v /var/tmp/$linuxdeploy . && chmod +x $linuxdeploy)
     else
-        # TODO fetch both: ./linuxdeploy-x86_64.AppImage ./linuxdeploy-plugin-qt-x86_64.AppImage
-        (cd /usr/local/bin && wget https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20220822-1/$linuxdeploy && chmod +x $linuxdeploy)
-        (cd /usr/local/bin && wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage && chmod +x linuxdeploy-plugin-qt-x86_64.AppImage)
         # https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20220822-1/linuxdeploy-x86_64.AppImage
         # https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
+        (cd /usr/local/bin && wget -nv https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/$linuxdeploy && chmod +x $linuxdeploy)
+    fi
+    # linuxdeploy Qt plugin
+    if [ -x "/usr/local/bin/linuxdeploy-plugin-qt-x86_64.AppImage" ]; then
+        :
+    elif [ -f "/var/tmp/linuxdeploy-plugin-qt-x86_64.AppImage" ]; then
+        (cd /usr/local/bin && cp -v /var/tmp/linuxdeploy-plugin-qt-x86_64.AppImage . && chmod +x linuxdeploy-plugin-qt-x86_64.AppImage)
+    else
+        # https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20220822-1/linuxdeploy-x86_64.AppImage
+        # https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
+        (cd /usr/local/bin && wget -nv https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage && chmod +x linuxdeploy-plugin-qt-x86_64.AppImage)
     fi
 fi
+echo -n "= "
 which $linuxdeploy
+echo
 
 # Install other dependencies TODO separate script
 if [ -n "$INSTALL_DEBIAN" ]; then
+    echo "installing Debian packages: $INSTALL_DEBIAN"
     apt-get install $INSTALL_DEBIAN || exit $?
 fi
+echo
 
 # Build application and copy it to AppDir/
 pro_file=$(find . -mindepth 1 -maxdepth 1 -name "*.pro")
 if [ -n "$pro_file" ]; then
     # qmake
     echo "BUILD - QMAKE: $pro_file"
-
-    # IDEA build_workspace_qmake.sh
 
     qmake || exit $?
     #qmake CONFIG+=release PREFIX=/usr || exit $?
@@ -62,13 +82,13 @@ if [ -n "$pro_file" ]; then
     INSTALL_ROOT=./AppDir make install || exit $?
     # https://docs.appimage.org/
     if [ -f "$icon_file" ]; then
-        cp -v "$icon_file" AppDir/
+        cp -vf "$icon_file" AppDir/
     fi
     if [ -f "$desktop_file" ]; then
-        cp -v "$desktop_file" AppDir/
+        cp -vf "$desktop_file" AppDir/
     fi
     if [ -f "$font_file" ]; then
-        cp -v "$font_file" AppDir/
+        cp -vf "$font_file" AppDir/
     fi
 
     echo "$pro_file done; bin/:"
@@ -90,11 +110,6 @@ fi
 # Run linuxdeploy tool to create AppImage
 if (which $linuxdeploy && ls AppDir) >/dev/null 2>&1; then
 
-    # Copy Qt plugins, if requested
-    if [ -n "$ADD_QT_PLUGINS" ]; then
-        mkdir -p AppDir/usr/bin/
-        cp -var $QTDIR/plugins/platforms/ AppDir/usr/bin/
-    fi
     # Copy font file
     if [ -n "$font_file" ]; then
         mkdir -p AppDir/usr/lib/fonts
@@ -103,8 +118,6 @@ if (which $linuxdeploy && ls AppDir) >/dev/null 2>&1; then
         mkdir -p AppDir/usr/lib/fonts
         cp -v res/*.ttf AppDir/usr/lib/fonts/
     fi
-
-    # 
 
     # Arguments
     args=()
