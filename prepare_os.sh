174701db2bcc2ce77d23ee81b5521ffad54f52a6
Philip Seeger <philip@c0xc.net>
2022-11-02 14:43:50 +0100

    test

--- prepare_os.sh
+++ prepare_os.sh
@@ -10,6 +10,10 @@ if [ -n "$INPUT_RECIPE" ]; then
     source "$INPUT_RECIPE"
 fi
 
+echo "PREP TEST:"
+env
+echo "INSTALL_DEBIAN = $INSTALL_DEBIAN"
+
 # Install other system dependencies
 if [ -n "$INSTALL_DEBIAN" ]; then
     echo "installing Debian packages: $INSTALL_DEBIAN"
